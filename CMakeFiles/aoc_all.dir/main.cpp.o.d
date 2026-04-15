c2f061fcbb5192240eb531c5a5dacc4289784fcb
leggi112 <jonathan-kastlmeier@web.de>
2026-04-15 16:20:07 +0200

    reset the repo

--- CMakeFiles/aoc_all.dir/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/aoc_all.dir/main.cpp.o: \
- /home/joni/Schreibtisch/AdventofCode/main.cpp /usr/include/stdc-predef.h \
- /home/joni/Schreibtisch/AdventofCode/AoC25/year.hpp
--- aoc_utils/CMakeFiles/aoc_utils.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libaoc_utils.a"
-)
