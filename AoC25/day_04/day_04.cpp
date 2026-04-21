5e7a0045cb975181a6403cf35cb3d8315b46c27f
leggi112 <jonathan-kastlmeier@web.de>
2026-04-21 11:58:29 +0200

    solved day 4.25

--- AoC25/day_04/day_04.cpp
+++ AoC25/day_04/day_04.cpp
@@ -6,6 +6,9 @@
 #include <cinttypes>
 #include <string>
 
+long solve_without_replace_4_25(std::vector<std::string>& puzzle);
+long solve_with_replace_4_25(std::vector<std::string>& puzzle);
+
 void AoC25::Day04::run(){
 	std::cout << "Day 4 in 2025:" << std::endl;
 
@@ -17,14 +20,80 @@ void AoC25::Day04::run(){
 }
 
 long AoC25::Day04::solve_part_one(){
-	return -1;
+	auto puzzle = AoC25::Day04::get_puzzle();
+	return solve_without_replace_4_25(puzzle);
 }
 
 long AoC25::Day04::solve_part_two(){
-	return -1;
+	auto puzzle = AoC25::Day04::get_puzzle();
+	auto last_puzzle = puzzle;
+	long result = 0;
+
+	do {
+		last_puzzle = puzzle;
+		result += solve_with_replace_4_25(puzzle);
+
+	} while (puzzle != last_puzzle);
+	
+	return result;
 }
 
 // Helpers for the day
 std::vector<std::string> AoC25::Day04::get_puzzle(){
 	return utils::read_puzzle("./AoC25/day_04/puzzle.txt");
 }
+
+long solve_without_replace_4_25(std::vector<std::string>& puzzle){
+	long result = 0;
+	for (size_t i  = 0; i < puzzle.size(); i++){
+		for (size_t j = 0; j < puzzle[i].length(); j++){
+			if (puzzle[i][j] != '@'){continue;}
+			
+			int x, y;
+			int neighbours = 0;
+
+			for (int n = 0; n < 9; n++){
+				if (n == 4){continue;}
+				x = j + (n % 3) - 1;
+				y = i + (n / 3) - 1;
+				if (x < 0 || x >= puzzle[i].length()){continue;}
+				if (y < 0 || y >= puzzle.size()){continue;}
+
+				if (puzzle[y][x] == '@'){neighbours++;}
+			}
+			if (neighbours < 4){result++;}
+		}
+	}
+	return result;
+}
+
+long solve_with_replace_4_25(std::vector<std::string>& puzzle){
+	long result = 0;
+	auto new_puzzle = puzzle;
+	for (size_t i  = 0; i < puzzle.size(); i++){
+		for (size_t j = 0; j < puzzle[i].length(); j++){
+			if (puzzle[i][j] != '@'){continue;}
+			
+			int x, y;
+			int neighbours = 0;
+
+			for (int n = 0; n < 9; n++){
+				if (n == 4){continue;}
+				x = j + (n % 3) - 1;
+				y = i + (n / 3) - 1;
+				if (x < 0 || x >= puzzle[i].length()){continue;}
+				if (y < 0 || y >= puzzle.size()){continue;}
+
+				if (puzzle[y][x] == '@'){neighbours++;}
+			}
+			if (neighbours < 4){
+				result++;
+				new_puzzle[i][j] = '.';
+			}
+		}
+	}
+	puzzle = new_puzzle;
+	return result;
+}
+
+
